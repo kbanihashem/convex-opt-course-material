2337134aff23b58b93620be46c9a6adad7e24649
Kiarash Banihashem <k.banihashem73@gmail.com>
2020-06-12 21:57:54 +0430

    worked on q1

--- pset15/q1.py
+++ pset15/q1.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cvxpy as cp
-from data.ls_perm_meas_data import m, k, n, A, y, x_true
+from data.ls_perm_meas_data import m, k, n, A, y, x_true, P
+K_P = P
+del P
 x_true = np.reshape(x_true, n)
 y = np.reshape(y, m)
 np.set_printoptions(suppress=True)
@@ -11,15 +13,21 @@ def get_error(y, y_hat):
 before = []
 after = []
 
-permuation = np.arange(m)
-x = cp.Variable(n)
-y_hat = A @ x
-error = (y_hat - y)**2
-obj = cp.Minimize(cp.sum(error))
-problem = cp.Problem(obj, [])
-problem.solve()
-x_unif = x.value
-bad_indexes = np.argsort(-error.value)[:k]
+bad_indexes = set()
+max_iter = 10
+for iteration in range(max_iter):
+    mask = np.array([i not in bad_indexes for i in range(m)])
+    x = cp.Variable(n)
+    y_hat = A @ x
+    error = (y_hat - y)**2
+    obj = cp.Minimize(cp.sum(error[mask]))
+    problem = cp.Problem(obj, [])
+    problem.solve()
+    #x is uniform the first time!
+    if iteration == 0:
+        x_unif = x.value
+    bad_indexes = np.argsort(-error.value)[:k]
+    bad_indexes_set = set(bad_indexes)
 
 y_hat_bad_indexs_sorted_index = np.argsort(y_hat.value[bad_indexes])
 y_true_bad_indexs_sorted_index = np.argsort(y[bad_indexes])
@@ -41,3 +49,8 @@ x_smart = x.value
 
 print(f"||x_unif - x_true||_2 = {np.linalg.norm(x_unif - x_true)}")
 print(f"||x_smart - x_true||_2 = {np.linalg.norm(x_smart - x_true)}")
+print(f"our estimate of permutated indexes = {sorted(bad_indexes)}")
+real_bad = [i for i in range(m) if abs(P[i,i] - 1) > 1/10]
+print(f"real permutated indexes = {real_bad}")
+correct_count = len(set(real_bad).intersection(bad_indexes_set))
+print(f"got {correct_count}/{k} correct")
