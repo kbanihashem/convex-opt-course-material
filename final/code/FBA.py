7f67fdec9bfc0f436ad4555e7e98e7f0b751fa4d
Kiarash Banihashem <k.banihashem73@gmail.com>
2020-07-20 20:02:18 +0430

    getting better with FBA

--- final/code/FBA.py
+++ final/code/FBA.py
@@ -1,46 +1,51 @@
 import json
 import numpy as np
 import cvxpy as cp
-def get_data():
-with open("Recon3D.json", "r") as file_handle:
-    dictionary = json.load(file_handle);
-    data = dictionary["reactions"];
-    temp_name = [(data[i])["name"] for i in range(len(data))];
-    b = [i for i in range(len(data)) if temp_name[i] == "Generic Human Biomass Reaction"];
-    b = b[0];
-    order = [i for j in (range(b), range(b+1, len(data)), range(b,b+1)) for i in j];
-    name = [(data[i])["name"] for i in order];
-    lower_bound = [(data[i])["lower_bound"] for i in order];
-    upper_bound = [(data[i])["upper_bound"] for i in order];
-    subsystem = [(data[i])["subsystem"] for i in order];
-    metabolites = [(data[i])["metabolites"] for i in order];
-    id = [((dictionary["metabolites"])[i])["id"] for i in range(len(dictionary["metabolites"]))];
-    S = np.zeros((len(id), len(metabolites)));
-    for i in range(len(metabolites)):
-        for j in range(len(id)):
-            if id[j] in metabolites[i].keys():
-                S[j, i] = metabolites[i][id[j]];
-
-def solve_with_removed_index(removed=None, solver=None):
-    solver = solver if solver is not None else cp.ECOS
-    u = np.array(upper_bound.copy())
-    l = np.array(lower_bound.copy())
-    if removed is not None:
-        removed = np.array(removed)
-        u[removed] = 0
-        l[removed] = 0
-    m, n = S.shape
-    v = cp.Variable(n)
-    #part a
-    constraints = [
-            S @ v == 0,
-            v <= u,
-            v >= l,
+def read_data():
+    with open("Recon3D.json", "r") as file_handle:
+        dictionary = json.load(file_handle);
+        data = dictionary["reactions"];
+        temp_name = [(data[i])["name"] for i in range(len(data))];
+        b = [i for i in range(len(data)) if temp_name[i] == "Generic Human Biomass Reaction"];
+        b = b[0];
+        order = [i for j in (range(b), range(b+1, len(data)), range(b,b+1)) for i in j];
+        name = [(data[i])["name"] for i in order];
+        lower_bound = [(data[i])["lower_bound"] for i in order];
+        upper_bound = [(data[i])["upper_bound"] for i in order];
+        subsystem = [(data[i])["subsystem"] for i in order];
+        metabolites = [(data[i])["metabolites"] for i in order];
+        id = [((dictionary["metabolites"])[i])["id"] for i in range(len(dictionary["metabolites"]))];
+        S = np.zeros((len(id), len(metabolites)));
+        for i in range(len(metabolites)):
+            for j in range(len(id)):
+                if id[j] in metabolites[i].keys():
+                    S[j, i] = metabolites[i][id[j]];
+    knockout_names = [
+            'Transport, nuclear',
+            'Fatty acid oxidation',
             ]
-    obj = cp.Maximize(v[-1])
-    problem = cp.Problem(obj, constraints)
-    ans = problem.solve(solver=solver)
-    return problem, v.value
+
+def solve_with_removed_index(removed=None):
+    solvers = {'ECOS': cp.ECOS, 'QSOP': cp.OSQP, 'SCS': cp.SCS}
+    for solver_name, solver in solvers.items():
+        u = np.array(upper_bound.copy())
+        l = np.array(lower_bound.copy())
+        if removed is not None:
+            removed = np.array(removed)
+            u[removed] = 0
+            l[removed] = 0
+        m, n = S.shape
+        v = cp.Variable(n)
+        constraints = [
+                S @ v == 0,
+                v <= u,
+                v >= l,
+                ]
+        obj = cp.Maximize(v[-1])
+        problem = cp.Problem(obj, constraints)
+        ans = problem.solve(solver=solver)
+        if problem.status == 'optimal':
+            return problem, v.value, solver_name
 
 def get_knockout_index(knockout):
     return [i for i, sub_name in enumerate(subsystem) if sub_name in knockout]
@@ -48,8 +53,17 @@ def get_knockout_index(knockout):
 def get_subystem_indexes(name):
     return get_knockout_index({name})
 
-knockout = set([
-        'Transport, nuclear',
-        'Fatty acid oxidation',
-        ])
+def part_a():
+    #should run read_data before this
+    global p_a, v_a, solver_a
+    p_a, v_a, solver_a = solve_with_removed_index()
+
+def evaluated_diff(v_w, v_tilde):
+    return (v_w[-1] - v_tilde[-1]) / v_w[-1]
+
+def part_b():
+    global p_transport, v_transport, solver_transport
+    global p_fatty, v_fatty, solver_fatty
+    p_transport, v_transport, solver_transport = solve_with_removed_index(get_subystem_indexes(knockout_names[0]))
+    p_fatty, v_fatty, solver_fatty = solve_with_removed_index(get_subystem_indexes(knockout_names[1]))
 
--- final/code/q6.py
+++ final/code/q6.py
@@ -5,6 +5,17 @@ n = 3
 u = 2
 gamma = 2
 G = (np.ones((n, n)) - np.identity(n)) / 2
-d = cp.Variable(n, nonneg=True)
-k = cp.Variable(n, nonneg=True)
-
+d = cp.Variable(n)
+k = cp.Variable(n)
+A = cp.Variable((n, n))
+constraints = [
+        d >= 0,
+        k >= 0,
+        A == cp.diag(d) + cp.diag(k) * G,
+        gamma * cp.sum(cp.inv_pos(d)) + cp.sum(cp.inv_pos(k)) <= u,
+        ]
+obj = cp.Minimize(cp.lambda_max(A))
+problem = cp.Problem(obj, constraints)
+problem.solve()
+print(problem.status)
+print(problem.value)
